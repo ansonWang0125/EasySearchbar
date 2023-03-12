a5b7b88198c7db6bd31558dcf4fe823289271c60
ansonWang0125 <b09705044@ntu.edu.tw>
2023-03-13 01:14:57 +0800

    second commit

--- ui/src/axios/api.js
+++ ui/src/axios/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const studentRequest = axios.create({
-    // baseURL: 'https://post-articles.onrender.com/api/env',
     baseURL: 'http://localhost:9090/api/enrollment'
  });
 
